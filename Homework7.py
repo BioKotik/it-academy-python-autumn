a2a9caf73074aa048ef84e092d0bfb2b98761314
BioKotik <i.kachan@list.ru>
2019-11-27 19:52:55 +0300

    Add class Person

--- Homework7.py
+++ Homework7.py
@@ -59,52 +59,61 @@ class Goblin(GameObject):
         self._desc = value
 
 
-def hit(noun):
-    if noun in GameObject.objects:
-        thing = GameObject.objects[noun]
-        if type(thing) == Rogue:
-            thing.health = thing.health - 1
-            if thing.health <= 0:
-                msg = "You killed the Rogue!"
+class Person(GameObject):
+    def __init__(self, name):
+        self.class_name = "person"
+        self.desc = "It's you"
+        self.health = 5
+        super().__init__(name)
+
+    @staticmethod
+    def hit(noun):
+        if noun in GameObject.objects:
+            thing = GameObject.objects[noun]
+            if type(thing) == Rogue:
+                thing.health = thing.health - 1
+                if thing.health <= 0:
+                    msg = "You killed the Rogue!"
+                else:
+                    msg = "You hit the {}".format(thing.class_name)
             else:
-                msg = "You hit the {}".format(thing.class_name)
+                msg = "There is no {} here".format(noun)
+            return msg
+
+    @staticmethod
+    def examine(noun):
+        if noun in GameObject.objects:
+            return GameObject.objects[noun].get_desc()
         else:
-            msg = "There is no {} here".format(noun)
-        return msg
+            return "There is no {} here.".format(noun)
 
+    @staticmethod
+    def get_input():
+        command = input(": ").split()
+        verb_word = command[0]
+        if verb_word in verb_dict:
+            verb = verb_dict[verb_word]
+        else:
+            print("Unknown verb {}". format(verb_word))
+            return
 
-def examine(noun):
-    if noun in GameObject.objects:
-        return GameObject.objects[noun].get_desc()
-    else:
-        return "There is no {} here.".format(noun)
+        if len(command) >= 2:
+            noun_word = command[1]
+            print(verb(noun_word))
+        else:
+            print(verb())
 
-
-def get_input():
-    command = input(": ").split()
-    verb_word = command[0]
-    if verb_word in verb_dict:
-        verb = verb_dict[verb_word]
-    else:
-        print("Unknown verb {}". format(verb_word))
-        return
-
-    if len(command) >= 2:
-        noun_word = command[1]
-        print(verb(noun_word))
-    else:
-        print(verb())
-
-
-def say(noun):
-    return 'You said "{}"'.format(noun)
+    @staticmethod
+    def say(noun):
+        return 'You said "{}"'.format(noun)
 
 
 verb_dict = {
-    "say": say,
-    "examine": examine,
-    "hit": hit,
+    "say": Person.say,
+    "examine": Person.examine,
+    "hit": Person.hit,
 }
+
 rogue = Rogue("Josh the rogue")
 goblin = Goblin("Little goblin")
 
@@ -116,5 +125,7 @@ print("Hi. It's me, voice in your head. I help you, to survive." + "\n"
       "(like 'hit goblin', kill 'em all c:). And last, you can" + "\n"
       "examine your enemies. Just say 'examine' and your enemy." + "\n"
       "Then you will know all about your enemy. It's all. Good Luck")
+
+person = Person(input("Please, input your name" + "\n"))
 while True:
-    get_input()
+    Person.get_input()
